60ca713c212d5dfdfd07bc4c9a832f9fac27d0c6
tgrsnts <tegarsantoso72@gmail.com>
2024-05-26 00:25:42 +0700

    second commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                'poppins': ['"Poppins"'],
+            },
+            colors: {
+                'first': '#6F4E37',
+                'second': '#A67B5B',
+                'primary': '#ECB176',
+                'secondary' : 'FED8B1'
+            }
+        }
+    },
+    variants: {
+        extend: {
+            visibility: ["group-hover"],
+        },
+    },
+}
