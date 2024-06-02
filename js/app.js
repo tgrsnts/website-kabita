e0121a946e624001bd02f327de2efe4d60910c41
tgrsnts <tegarsantoso72@gmail.com>
2024-06-02 21:15:40 +0700

    form login and register

--- js/app.js
+++ js/app.js
@@ -13,3 +13,20 @@ function toggleMenu() {
         navbarOpenIcon.classList.remove("hidden");
     }
 }
+
+// Untuk Hidden Password atau View Password
+const passwordToggle = document.querySelector('.js-password-toggle')
+        passwordToggle.addEventListener('change', function () {
+            const password = document.querySelector('.js-password'),
+                passwordLabel = document.querySelector('.js-password-label')
+
+            if (password.type === 'password') {
+                password.type = 'text'
+                passwordLabel.innerHTML = '<i class="fa-solid fa-eye-slash"></i>'
+            } else {
+                password.type = 'password'
+                passwordLabel.innerHTML = '<i class="fa-solid fa-eye"></i>'
+            }
+
+            password.focus()
+        })
